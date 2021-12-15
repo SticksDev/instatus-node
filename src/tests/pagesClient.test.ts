c3a29d6d0594c46f4437d6fbb744299c51ee19b1
sticksdev <tsommers150@gmail.com>
2021-12-14 21:37:27 -0500

    fix typeof

--- src/tests/pagesClient.test.ts
+++ src/tests/pagesClient.test.ts
@@ -4,7 +4,7 @@ import Client from "../index"
 
 describe("Client.pageRequest()", () => {
     it("should return SyntaxError when attempting to make a request that is not a vaild typeof Client#pageRequest (CASE_TYPEOFERR)", () => {
-        Client.pageRequest('doesnotexist').catch((err) => {
+        Client.pageRequest('doesnotexist' as any).catch((err) => {
             expect(err).toEqual(`doesnotexist is not a vaild typeof Client#pageRequest`)
         })
     })
